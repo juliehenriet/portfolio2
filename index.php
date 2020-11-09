1d46ed9db829712823181c474b22aaefb008497d
julie <juliehenriet@gmail.com>
2020-11-09 18:10:02 +0100

    modif loader

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
+<?php require('../portfolio2/partials/header.php');?>
 <main>
 <div id="loader-wrapper">
     <div id="loader"></div>
  
-    <div class="loader-section section-left"></div>
-    <div class="loader-section section-right"></div>
+    <!-- <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div> -->
  
 </div>
 </main>
+<?php require('../portfolio2/partials/footer.php');?>
