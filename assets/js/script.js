99f0dece6dd60068d4fe2674cac4348bca62e872
julie <juliehenriet@gmail.com>
2020-11-09 21:14:34 +0100

    animation transition owl

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 const loader = document.getElementById("loader-wrapper");
+const owl = document.getElementById("owl");
+const main_article = document.getElementById("main-article");
 
-window.addEventListener('load', function(){
+main_article.style.display = "none";
 
-});
+
+
+function loader_setTime() {
+
+    const p = document.createElement('p');
+    loader.appendChild(p);
+    p.innerText = "Loading ..."
+    
+    if (owl.style.opacity <= 1) {
+        setTimeout(() => {
+            owl.style.opacity = 1;
+        }, 500);
+    }
+}
+loader_setTime();
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<?php require('../portfolio2/partials/header.php');?>
+<?php require('../portfolio2/partials/header.php'); ?>
 <main>
-<div id="loader-wrapper">
-    <div id="loader"></div>
-        <img src="./assets/img/owl.png" alt="hiboux en vole">
-    <!-- <div class="loader-section section-left"></div>
-    <div class="loader-section section-right"></div> -->
- 
-</div>
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+        <img src="./assets/img/owl.png" alt="hiboux en vole" id="owl">
+       
+    </div>
+
+    <article class="main-article" id ="main-article">
+        <!-- <h1>Coucou</h1> -->
+    </article>
+
 </main>
-<?php require('../portfolio2/partials/footer.php');?>
+<?php require('../portfolio2/partials/footer.php'); ?>
