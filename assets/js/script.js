5831555b80d9f919d2482382eb7939ff19c362da
julie <juliehenriet@gmail.com>
2020-11-09 18:45:16 +0100

    .

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const loader = document.getElementById("loader-wrapper");
+
+window.addEventListener('load', function(){
+
+});
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <main>
 <div id="loader-wrapper">
     <div id="loader"></div>
- 
+        <img src="./assets/img/owl.png" alt="hiboux en vole">
     <!-- <div class="loader-section section-left"></div>
     <div class="loader-section section-right"></div> -->
  
