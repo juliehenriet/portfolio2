09d125ce95cf831515d731b26fe1fc5efdfdb6ef
julie <juliehenriet@gmail.com>
2020-11-09 23:37:50 +0100

    texte loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,62 @@ main_article.style.display = "none";
 
 
 
-function loader_setTime() {
+// function loader_setTime() {
 
-    const p = document.createElement('p');
-    loader.appendChild(p);
-    p.innerText = "Loading ..."
-    
-    if (owl.style.opacity <= 1) {
-        setTimeout(() => {
-            owl.style.opacity = 1;
-        }, 500);
-    }
+
+//     const p = document.createElement('p');
+//     p.classList.add('loading-text')
+//     loader.appendChild(p);
+
+//     p.innerText = 'Loading ...'
+//     const p_text = document.querySelectorAll('loading-text');
+//     //------------------p retourne un tableau ------------//
+
+//     if (owl.style.opacity <= 1) {
+//         setTimeout(() => {
+//             owl.style.opacity = 1;
+//         }, 500);
+
+//         setInterval(() => {
+
+//             for (let a = 0; a <= p_text.length; a++) {
+//                 // console.log(p_text.length)
+//             }
+
+//         }, 100);
+
+//     }
+
+
+// }
+// loader_setTime();
+
+
+//     //-------------------je veux que mon texte s'écrit tout seul dans un petit laps de temps -----------//
+//     //--------------------------je créer mon texte et je l'ajoute à mon loader-------------------------//
+
+const p = document.createElement('p');
+p.classList.add('loading-text');
+loader.appendChild(p);
+// p.innerText = 'Loading ...';
+const p_text = "loading...";
+
+//------------------p_text retourne un tableau--------------//
+
+const words = p_text.split('');
+
+//------------------on parcours le tableau pour avoir chaque lettre--------------//
+
+function writte() {
+
+
+    setInterval(()=>{
+        if(words.length>0){
+            p.innerText += words.shift();
+        }
+    },70);
+  
+
 }
-loader_setTime();
+writte();
+//------------------netotyer le set interval--------------//
