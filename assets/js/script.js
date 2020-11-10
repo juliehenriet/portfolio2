b94f6888d768458385721a9e5c479a0616026fb3
julie <juliehenriet@gmail.com>
2020-11-10 21:35:56 +0100

    inline loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,20 @@
 const loader = document.getElementById("loader-wrapper");
 const owl = document.getElementById("owl");
 const main_article = document.getElementById("main-article");
-
-main_article.style.display = "none";
-
-
-
 //     //-------------------je veux que mon texte s'écrit tout seul dans un petit laps de temps -----------//
 //     //--------------------------je créer mon texte et je l'ajoute à mon loader-------------------------//
-
-
+const load_div = document.createElement('div');
+load_div.classList.add('container_loader');
 const p = document.createElement('p');
 p.classList.add('loading-text');
-loader.appendChild(p);
+loader.appendChild(load_div);
+load_div.appendChild(p);
 let count = 0;
-// p.innerText = 'Loading ...';
-const p_text = "loading ...";
-
+const p_text = "Loading ...";
 const counter = document.createElement('p');
 counter.classList.add('count');
-loader.appendChild(counter);
+load_div.appendChild(counter);
+
 //------------------p_text retourne un tableau--------------//
 
 const words = p_text.split('');
@@ -32,27 +27,23 @@ function writte() {
         if (words.length > 0) {
             p.innerText += words.shift();
         }
+            owl.style.opacity = 1;
+           
 
-        let opacity = 0;
-
-        if (opacity <= 1) {
-            opacity +=0.1;
-            owl.style.opacity = opacity;
-        }
     }, 70);
 
     setInterval(() => {
         
         if (count <= 99) {
-            ++count;
-            console.log(count)
-            counter.innerText = count + "%";
+            count++;
+            counter.innerText = " " + count + "%";
            
         }
 
     
-    }, 20);
+    }, 10);
 
 }
 writte();
-//------------------netotyer le set interval--------------//
+
+main_article.style.display = "none";
