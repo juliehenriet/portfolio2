7b83eaa086687a55278356d0ac52840dc93a1391
julie <juliehenriet@gmail.com>
2020-11-12 20:59:13 +0100

    add owl logo

--- index.php
+++ index.php
@@ -6,17 +6,25 @@
     </div>
 
     <article id="main-article">
-
-       <section id="section-aside">
+        <section id="section-aside">
+            <div id="about-me">
+                <img src="./assets/img/hiboux-logo.png" alt="logo géométrique d'un hiboux blanc avec des tâche de peinture bleu en dessous" class="owl-logo">
+                <h1>Numérisons votre projet ensemble!</h1>
+                <p>Je m'appelle Julie j'ai 26 ans, je suis à la recherche d'un emploi en tant que développeuse front-end, ou d'un contrat en alternance.<br><br>
+                    Sur ce site vous allez trouver un aperçu de mon cv et de mes compétences, ainsi que quelques réalisations graphiques effectuées pour le plaisir ou lors de mes stages.<br><br>
+                    Si vous êtes intéressé par mon profil, vous pouvez me <b><a href="contact.php">contacter ici</a></b> et me laissez vos coordonnées.</p>
+                </p>
+                <div class="button-emergency"><a href="/contact.php">CONTACTEZ-MOI</a></div>
+            </div>
             <!-- <aside id="aside-left"><a href="contact.php/">Contact</a></aside>
             <aside id="aside-right"><a href="contact.php/">Réalisation</a></aside> -->
         </section>
         <img src="./assets/img/lille.jpg" class="home zoom">
-    </article> 
+    </article>
 
-       
 
-      
+
+
 
 </main>
 <?php require('../portfolio2/partials/footer.php'); ?>
